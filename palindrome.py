4e259bd685204479c794d4169dbbae9d4324d806
MuShaf-NMS <nurmuhammadshafa@gmail.com>
2019-09-27 19:19:52 +0800

    revisi-1

--- palindrome.py
+++ palindrome.py
@@ -1,18 +1,24 @@
-f = open('data.txt')
-l = f.readlines()
-data = []
-h = 0
-for i in l:
-  data.append(i.rstrip().split(' '))
-for i in range(len(data)):
-  if data[i] == ['']:
-    continue
-  for j in data[i]:
-    a = j
-    b = ''
-    for k in range(len(j)-1,-1,-1):
-      b += j[k]
-    if a == b:
-      h += 1
-print('jumlah kata yang bacaannya sama dari kiri ke kanan dan sebalikny dalam file adalah',h)
-
+def baca_file(a):
+    f = open(a)
+    l = f.readlines()
+    data = []
+    for i in l:
+        data.append(i.rstrip().split(' '))
+    return data
+def kata(kt):
+    h = 0
+    for i in range(len(kt)):
+        if kt[i] == ['']:
+            continue
+        for j in kt[i]:
+            a = j
+            b = ''
+            for k in range(len(j)-1,-1,-1):
+                b += j[k]
+                if a == b:
+                    h += 1
+    return h
+data = 'data.txt'
+Kata = baca_file(data)
+hasil = kata(Kata)
+print('jumlah kata yang bacaannya sama dari kiri ke kanan dan sebalikny dalam file adalah',hasil)
